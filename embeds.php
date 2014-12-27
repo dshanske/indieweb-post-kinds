5314357699b2de13ef28f3c09df9db4fefb4460c
David Shanske <david@shanske.com>
2014-12-26 19:20:30 -0500

    Only Poll OEmbed for Selected Sites - Temporary measure before implementing caching

--- embeds.php
+++ embeds.php
@@ -4,17 +4,6 @@
 // planning on using wp embed register handler to register these for embedding
 // Then use wp_oembed_get to embed posts from any supported site, and if the site isn't supported, mark it up manually
 
-wp_embed_register_handler( 'facebook_post', '/https:\/\/www\.facebook\.com\/[a-z]+\/posts\//i', 'wp_handler_facebook' );
-
-function wp_handler_facebook( $matches, $attr, $url, $rawattr )
-   {
-      $embed = '<div id="fb-root"></div>';
-      $embed .= '<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, \'script\', \'facebook-jssdk\'));</script>';
-      $embed .= '<div class="fb-post" data-href="' . esc_url($url) . '" data-width="466"><div class="fb-xfbml-parse-ignore"><a href="' . esc_url($url) .  '">Post</a></div></div>';
-      return apply_filters( 'embed_facebook', $embed, $matches, $attr, $url, $rawattr );
-
-   }
-
 function get_embed_facebook ($url)
    {
       $embed = '<div id="fb-root"></div>';
@@ -32,7 +21,7 @@ function get_embed_gplus ($url)
         return $embed;
    }
 
-function embed_instagram ($url)
+function get_embed_instagram ($url)
    {
         $embed = '<iframe src="';
 	$embed .= esc_url($url) .  'embed" width="612" height="710" frameborder="0" scrolling="no" allowtransparency="true"></iframe>';
@@ -40,18 +29,35 @@ function embed_instagram ($url)
    }
 
 function new_embed_get ($url) {
-      $embed = wp_oembed_get ($url);
-      if ($embed == false)
-	{
-      		if (strpos($url,'https://www.facebook.com/') !== false) {
-	       		$embed = get_embed_facebook ($url);
-			}
-      		elseif (strpos($url,'https://plus.google.com/') !== false) {
-	       		$embed = get_embed_gplus ($url);
-			}
-      		elseif (strpos($url,'https://instagram.com/') !== false) {
-       	       		$embed = get_embed_instagram ($url);
-			}
+      $host = extract_domain_name($url);
+      switch ($host)
+	 {
+		case 'facebook.com':
+      	       	     $embed = get_embed_facebook ($url);
+		break;
+		case 'plus.google.com':
+		     $embed = get_embed_gplus ($url);
+		break;
+		case 'instagram.com':
+		     $embed = wp_oembed_get ($url);
+		break;
+		case 'twitter.com':
+		     $embed = wp_oembed_get ($url);
+		break;
+		case 'youtube.com':
+		     $embed = wp_oembed_get ($url);
+		break;
+                case 'hulu.com':
+                     $embed = wp_oembed_get ($url);
+                break;
+                case 'slideshare.net':
+                     $embed = wp_oembed_get ($url);
+                break;
+                case 'smugmug.com':
+                     $embed = wp_oembed_get ($url);
+                break;
+		default:
+		    $embed = false;
 	}
       if (wp_is_mobile()) { $embed = false; }
       return $embed;
