d6e2ff762e345c992733e4d74d11539aa8a21b4f
David Shanske <david@shanske.com>
2015-07-24 14:44:07 -0400

    Clean Up Main File

--- indieweb-post-kinds.php
+++ indieweb-post-kinds.php
@@ -3,8 +3,7 @@
  * Post Kinds
  *
  * @link http://indiewebcamp.com/Post_Kinds_Plugin
- */
-/**
+ * @package Post Kinds
  * Plugin Name: Post Kinds
  * Plugin URI: https://wordpress.org/plugins/indieweb-post-kinds/
  * Description: Ever want to reply to someone else's post with a post on your own site? Or to "like" someone else's post, but with your own site?
@@ -14,44 +13,44 @@
  * Text Domain: Post kinds
  */
 
-define ( 'POST_KINDS_VERSION', '2.0.2' );
+define( 'POST_KINDS_VERSION', '2.0.2' );
 
-load_plugin_textdomain( 'Post kind', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
+load_plugin_textdomain( 'Post kind', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 if ( ! defined( 'POST_KIND_INCLUDE' ) )
-    define( 'POST_KIND_INCLUDE' , '' );
+	define( 'POST_KIND_INCLUDE' , '' );
 
 if ( ! defined( 'MULTIKIND' ) )
-    define( 'MULTIKIND', '0' );
+	define( 'MULTIKIND', '0' );
 
-// Add Kind Taxonomy
+// Add Kind Taxonomy.
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-kind-taxonomy.php' );
 
- // Config Settings
+ // Config Settings.
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-kind-config.php' );
 
-// Add a Settings Link to the Plugins Page
+// Add a Settings Link to the Plugins Page.
 $plugin = plugin_basename( __FILE__ );
 add_filter( 'plugin_action_links_$plugin', array( 'kind_config', 'settings_link' ) );
 
 
-// Add Kind Post Metadata
+// Add Kind Post Metadata.
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-kind-postmeta.php' );
 
-// Add Kind Core Functions
+// Add Kind Core Functions.
 require_once( plugin_dir_path( __FILE__ ) . '/includes/kind-functions.php' );
 // Add Kind Display Functions
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-kind-view.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-kind-display.php' );
 
-// Add Kind Meta Display Functions
+// Add Kind Meta Display Functions.
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-kind-meta.php' );
 
 if( ! class_exists ( 'ogp\Parser' ) ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'includes/class-ogp-parser.php' );
 	}
 
-// Load stylesheets
+// Load stylesheets.
 add_action( 'wp_enqueue_scripts', 'kindstyle_load' );
 add_action( 'admin_enqueue_scripts', 'kind_admin_style' );
 
@@ -63,7 +62,7 @@ function kind_admin_style() {
     wp_enqueue_style( 'kind-admin', plugins_url( 'css/kind-admin.min.css', __FILE__), array(), POST_KINDS_VERSION );
 }
 
-// Add a notice to the Admin Pages if the WordPress Webmentions Plugin isn't Activated
+// Add a notice to the Admin if the Webmentions Plugin isn't Activated.
 add_action( 'admin_notices', 'postkind_plugin_notice' );
 
 function postkind_plugin_notice() {
@@ -76,11 +75,11 @@ function postkind_plugin_notice() {
 }
 
 
-// Extracts the Domain Name for a URL for presentation purposes
+// Extracts the Domain Name for a URL for presentation purposes.
 if ( ! function_exists( 'extract_domain_name' ) ) {
     function extract_domain_name( $url ) {
       $host = parse_url( $url, PHP_URL_HOST );
-      $host = preg_replace( "/^www\./", "", $host );
+      $host = preg_replace( '/^www\./', '', $host );
       return $host;
     }
   }
@@ -114,6 +113,4 @@ if (!  function_exists( 'array_filter_recursive' ) ) {
 		unset( $value );
 		return $array;
 	}
-}
-
-?>
+} ?>
