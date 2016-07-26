90142b5d33ecd5803e1940b66b684b64d8561209
David Shanske <david@shanske.com>
2016-07-26 00:42:49 -0500

    Fix Twitter Shim Load Option

--- indieweb-post-kinds.php
+++ indieweb-post-kinds.php
@@ -7,7 +7,7 @@
  * Plugin Name: Post Kinds
  * Plugin URI: https://wordpress.org/plugins/indieweb-post-kinds/
  * Description: Ever want to reply to someone else's post with a post on your own site? Or to "like" someone else's post, but with your own site?
- * Version: 2.3.8
+ * Version: 2.4.0
  * Author: David Shanske
  * Author URI: https://david.shanske.com
  * Text Domain: Post kinds
@@ -20,7 +20,7 @@ if ( ! defined( 'MULTIKIND' ) ) {
 add_action( 'plugins_loaded', array( 'Post_Kinds_Plugin', 'init' ) );
 
 class Post_Kinds_Plugin {
-	public static $version = '2.3.8';
+	public static $version = '2.4.0';
 	public static function init() {
 		load_plugin_textdomain( 'Post kind', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 		// Add Kind Taxonomy.
@@ -32,6 +32,9 @@ class Post_Kinds_Plugin {
 		// On Activation, add terms.
 		register_activation_hook( __FILE__, array( 'Kind_Taxonomy', 'activate_kinds' ) );
 
+		// Add Kind Global Functions.
+		require_once plugin_dir_path( __FILE__ ) . '/includes/kind-functions.php';
+
 		// Config Settings.
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-kind-config.php';
 		add_action( 'init' , array( 'Kind_Config', 'init' ) );
@@ -39,42 +42,44 @@ class Post_Kinds_Plugin {
 		// Add a Settings Link to the Plugins Page.
 		$plugin = plugin_basename( __FILE__ );
 		add_filter( 'plugin_action_links_$plugin', array( 'Post_Kinds_Plugin', 'settings_link' ) );
-		
+
 		// Add Kind Post UI Configuration
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-kind-tabmeta.php';
 		add_action( 'init' , array( 'Kind_Tabmeta', 'init' ) );
 		Kind_Tabmeta::$version = self::$version;
 
-		// Add Kind Global Functions.
-		require_once plugin_dir_path( __FILE__ ) . '/includes/kind-functions.php';
-		
 		// Add Kind Display Functions.
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-kind-view.php';
 		add_action( 'init' , array( 'Kind_View', 'init' ) );
 
 		// Add Kind Meta Storage and Retrieval Functions.
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-kind-meta.php';
-		
+
 		// Add an MF2 Parser
 		if ( version_compare( PHP_VERSION, '5.3', '>' ) ) {
 			if ( ! class_exists( 'Mf2\Parser' ) ) {
 				require_once plugin_dir_path( __FILE__ ) . 'includes/Mf2/Parser.php';
+			}
+                        }
+		  if ( ! function_exists( 'Mf2\xpcs' ) ) {
 				require_once plugin_dir_path( __FILE__ ) . 'includes/Mf2/functions.php';
+			}
+			if ( ! class_exists( 'Mf2\Shim' ) ) {
 				require_once plugin_dir_path( __FILE__ ) . 'includes/Mf2/Twitter.php';
 			}
 		}
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-mf2-cleaner.php';
-		
+
 		// Add Link Preview Parsing
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-link-preview.php';
 		add_action( 'init' , array( 'Link_Preview', 'init' ) );
-		
+
 		// Load stylesheets.
 		add_action( 'wp_enqueue_scripts', array( 'Post_Kinds_Plugin', 'style_load' ) );
 		add_action( 'admin_enqueue_scripts', array( 'Post_Kinds_Plugin', 'admin_style_load' ) );
 	}
 
-  /**
+	/**
 	 * Adds link to Plugin Page for Options Page.
 	 *
 	 * @access public
@@ -86,7 +91,7 @@ class Post_Kinds_Plugin {
 		array_unshift( $links, $settings_link );
 		return $links;
 	}
-	
+
 	/**
 	 * Loads the Stylesheet for the Plugin.
 	 */
@@ -98,12 +103,12 @@ class Post_Kinds_Plugin {
 			wp_enqueue_style( 'kind', plugin_dir_url( __FILE__ ) . 'css/kind.themecompat.min.css', array(), self::$version );
 		}
 	}
-	
+
 	/**
 	 * Loads the Admin Stylesheet for the Plugin.
 	 */
 	public static function admin_style_load() {
-		wp_enqueue_style( 'kind-admin', plugin_dir_url( __FILE__ ) . 'css/kind.admin.min.css', array(), self::$version);
+		wp_enqueue_style( 'kind-admin', plugin_dir_url( __FILE__ ) . 'css/kind.admin.min.css', array(), self::$version );
 	}
 }
 
