84bb11e6bdc82c727f6737f1b6a350f8174b67b0
Michael Bishop <miklb@miklb.com>
2017-05-17 22:37:55 -0400

    basic kind-photo template w/support for multi-photos

---
+++ includes/views/kind-photo.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Photo Template
+ *
+ */
+
+ $kind = get_post_kind_slug( get_the_ID() );
+ $meta = new Kind_Meta( get_the_ID() );
+ $author = Kind_View::get_hcard( $meta->get_author() );
+ $site_name = Kind_View::get_site_name( $meta->get_cite(), $meta->get_url() );
+ $title = Kind_View::get_cite_title( $meta->get_cite(), $meta->get_url() );
+ $photos = $meta->get( 'photo' );
+
+?>
+<section>
+<header>
+<?php echo Kind_Taxonomy::get_icon( $kind );
+if( ! $embed ) {
+	if ( $title ) {
+		echo $title;
+	}
+	if ( $author ) {
+		echo ' ' . __( 'by', 'indieweb-post-kinds' ) . ' ' . $author;
+	}
+	if ( $site_name ) {
+		echo '<em>(<span class="p-publication">' . $site_name . '</span>)</em>';
+	}
+}
+?>
+</header>
+<p>
+<?php
+for ($i = 0; $i < count($photos); $i++) { ?>
+
+		<img src="<?php echo esc_url( $photos[$i] ); ?>" class="u-photo">
+<?php } ?>
+</p>
+</section>
+
+<?php
