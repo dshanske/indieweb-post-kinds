a9f0ffc0283e07b65befc38d2a05c4402afc4f58
David Shanske <david@shanske.com>
2017-11-04 07:15:21 -0500

    Add multi-array test

--- tests/test-mf2-post.php
+++ tests/test-mf2-post.php
@@ -29,5 +29,16 @@ class MF2PostTest extends WP_UnitTestCase {
 		$mf2_post = new MF2_Post( $post );
 		$this->assertEquals( '2016-01-01T12:01:23+00:00', $mf2_post->get( 'published', true ) );
 	}
+	public function test_set_and_get_multi_array() {
+		$post = self::factory()->post->create();
+		$mf2_post = new MF2_Post( $post );
+		$mf2_post->set( array(
+			'checkin' => 'Blah',
+			'in_reply_to' => 'Nothing'
+		) );
+		$mf2_post = new MF2_Post( $post );
+		$this->assertEquals( 'Blah', $mf2_post->get( 'checkin', true ) );
+	}
+
 }
 
