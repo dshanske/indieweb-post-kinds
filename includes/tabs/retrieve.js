e367d20e30cdf74e507d099c5f813a44633d7d00
David Shanske <david@shanske.com>
2017-12-12 00:05:53 -0600

    JS Coding Standards

--- includes/tabs/retrieve.js
+++ includes/tabs/retrieve.js
@@ -1,17 +1,21 @@
-jQuery( document ).on( 'click', '.kind-retrieve-button', function($) {
-	jQuery.ajax({ 
-			type: 'GET',
-	        	// Here we supply the endpoint url, as opposed to the action in the data object with the admin-ajax method
-			url: pk_link_preview.api_url + 'parse/', 
-			beforeSend: function ( xhr ) {
-			       	// Here we set a header 'X-WP-Nonce' with the nonce as opposed to the nonce in the data object with admin-ajax
-				xhr.setRequestHeader( 'X-WP-Nonce', pk_link_preview.api_nonce );
-			},
-			data: { 
-				kindurl: jQuery("#cite_url").val()
-			},
-		success : function( response ) {
-			if ( typeof response === 'undefined' ) {
+jQuery( document ).on( 'click', '.kind-retrieve-button', function( $ ) {
+	jQuery.ajax({
+		type: 'GET',
+
+		// Here we supply the endpoint url, as opposed to the action in the data object with the admin-ajax method
+		url: PKAPI.api_url + 'parse/',
+		beforeSend: function( xhr ) {
+
+		// Here we set a header 'X-WP-Nonce' with the nonce as opposed to the nonce in the data object with admin-ajax
+		xhr.setRequestHeader( 'X-WP-Nonce', PKAPI.api_nonce );
+		},
+		data: {
+			kindurl: jQuery( '#cite_url' ).val()
+		},
+		success: function( response ) {
+			var published;
+			var updated;
+			if ( 'undefined' === typeof response ) {
 				alert( 'Error: Unable to Retrieve' );
 				return;
 			}
@@ -20,56 +24,56 @@ jQuery( document ).on( 'click', '.kind-retrieve-button', function($) {
 				return;
 			}
 			if ( 'name' in response ) {
-				jQuery("#cite_name").val(response['name']);
-				if ( jQuery('#title').val() === '' ) {
-					jQuery("#title").val(response['name']);
+				jQuery( '#cite_name' ).val( response.name );
+				if ( '' === jQuery( '#title' ).val() ) {
+					jQuery( '#title' ).val( response.name );
 				}
 			}
- 		     	if ( 'publication' in response ) {
-				jQuery("#cite_publication").val(response['publication']) ;
-   	  	 	}
+			if ( 'publication' in response ) {
+				jQuery( '#cite_publication' ).val( response.publication ) ;
+			}
 			if ( 'published' in response ) {
-				var published = moment.parseZone( response['published'] );
-				jQuery("#cite_published_date").val( published.format('YYYY-MM-DD') ) ;
-				jQuery("#cite_published_time").val(published.format('HH:mm:ss') ) ;
-				jQuery("#cite_published_offset").val(published.format('Z') );
- 	 	    	}
-   		   	if ( 'updated' in response ) {
-				var updated = moment.parseZone( response['updated'] );
-        			jQuery("#cite_updated_date").val( updated.format('YYYY-MM-DD') ) ;
-        			jQuery("#cite_updated_time").val(updated.format('HH:mm:ss') ) ;
-        			jQuery("#cite_updated_offset").val(updated.format('Z') );  
-      			}
+				published = moment.parseZone( response.published );
+				jQuery( '#cite_published_date' ).val( published.format( 'YYYY-MM-DD' ) ) ;
+				jQuery( '#cite_published_time' ).val( published.format( 'HH:mm:ss' ) ) ;
+				jQuery( '#cite_published_offset' ).val( published.format( 'Z' ) );
+			}
+			if ( 'updated' in response ) {
+				updated = moment.parseZone( response.updated );
+				jQuery( '#cite_updated_date' ).val( updated.format( 'YYYY-MM-DD' ) ) ;
+				jQuery( '#cite_updated_time' ).val( updated.format( 'HH:mm:ss' ) ) ;
+				jQuery( '#cite_updated_offset' ).val( updated.format( 'Z' ) );
+			}
 			if ( 'summary' in response ) {
-        			jQuery("#cite_summary").val(response['summary']) ;
-      			}
-  	    		if ( 'featured' in response ) {
-  	 	     		jQuery("#cite_featured").val(response['featured']) ;
-      			}
-      			if ( ( 'author' in response ) && ( typeof response['author'] != 'string' ) ) {
-	      			if ( 'name' in response['author'] ) {
-  	      				jQuery("#cite_author_name").val(response['author']['name'].join(";") ) ;
-    		  		}
-        			if ( 'photo' in response['author'] ) {
-          			jQuery("#cite_author_photo").val(response['author']['photo'].join(";") ) ;
-        			}
-        			if ( 'url' in response['author'] ) {
-          			jQuery("#cite_author_url").val(response['author']['url'].join(";") ) ;
-        			}
+				jQuery( '#cite_summary' ).val( response.summary ) ;
+			}
+			if ( 'featured' in response ) {
+				jQuery( '#cite_featured' ).val( response.featured ) ;
+			}
+			if ( ( 'author' in response ) && ( 'string' != typeof response.author ) ) {
+				if ( 'name' in response.author ) {
+					jQuery( '#cite_author_name' ).val( response.author.name.join( ';' ) ) ;
+				}
+				if ( 'photo' in response.author ) {
+					jQuery( '#cite_author_photo' ).val( response.author.photo.join( ';' ) ) ;
+				}
+				if ( 'url' in response.author ) {
+					jQuery( '#cite_author_url' ).val( response.author.url.join( ';' ) ) ;
+				}
 			}
 			if ( 'category' in response ) {
-				jQuery("#cite_tags").val( response['category'].join(";") );
+				jQuery( '#cite_tags' ).val( response.category.join( ';' ) );
 			}
-		alert( pk_link_preview.success_message );
+		alert( PKAPI.success_message );
 		console.log( response );
 		},
-	  fail: function( response ) {
-		  	console.log( response );
-			alert(response.message);
+		fail: function( response ) {
+			console.log( response );
+			alert( response.message );
 		},
-	  error: function( jqXHR, textStatus, errorThrown) {
-	     	  alert( textStatus );
-		  console.log( jqXHR );
+		error: function( jqXHR, textStatus, errorThrown ) {
+			alert( textStatus );
+			console.log( jqXHR );
 		}
 	});
-})
+});
