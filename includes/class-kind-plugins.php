bf30e49524e2228789bf5577e9926380373f7b8d
Michael Bishop <miklb@miklb.com>
2018-08-31 22:33:40 -0400

    fix: update micropub filter for new changes
    fix is for master of wordpress-micropub plugin (2.0)

--- includes/class-kind-plugins.php
+++ includes/class-kind-plugins.php
@@ -17,7 +17,7 @@ class Kind_Plugins {
 		add_filter( 'semantic_linkbacks_post_type', array( 'Kind_Plugins', 'semantic_post_type' ), 11, 2 );
 
 		// Remove the Automatic Post Generation that the Micropub Plugin Offers
-		remove_filter( 'micropub_post_content', array( 'Micropub_Plugin', 'generate_post_content' ), 1, 2 );
+		remove_filter( 'micropub_post_content', array( 'Micropub_Render', 'generate_post_content' ), 1, 2 );
 
 	}
 
