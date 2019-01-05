410b8cc1b69b18335b6b5b39fe78156f087d6f2e
David Shanske <david@shanske.com>
2019-01-05 12:57:52 -0600

    Fix for PHP notice

--- indieweb-post-kinds.php
+++ indieweb-post-kinds.php
@@ -7,7 +7,7 @@
  * Plugin Name: Post Kinds
  * Plugin URI: https://wordpress.org/plugins/indieweb-post-kinds/
  * Description: Ever want to reply to someone else's post with a post on your own site? Or to "like" someone else's post, but with your own site?
- * Version: 3.1.6
+ * Version: 3.1.7
  * Author: David Shanske
  * Author URI: https://david.shanske.com
  * Text Domain: indieweb-post-kinds
@@ -28,7 +28,7 @@ add_action( 'plugins_loaded', array( 'Post_Kinds_Plugin', 'plugins_loaded' ) );
 add_action( 'init', array( 'Post_Kinds_Plugin', 'init' ) );
 
 class Post_Kinds_Plugin {
-	public static $version = '3.1.6';
+	public static $version = '3.1.7';
 	public static function init() {
 		// Add Kind Taxonomy.
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-post-kind.php';
@@ -80,8 +80,11 @@ class Post_Kinds_Plugin {
 
 		// Parse This
 		require_once plugin_dir_path( __FILE__ ) . 'includes/parse-this/parse-this.php';
-		require_once plugin_dir_path( __FILE__ ) . 'includes/parse-this/includes/class-mf2-post.php';
 
+		if ( ! class_exists( 'MF2_Post' ) ) {
+			require_once plugin_dir_path( __FILE__ ) . 'includes/parse-this/includes/class-mf2-post.php';
+		}
+		
 		// Kind Widgets
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-kind-menu-widget.php';
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-kind-post-widget.php';
