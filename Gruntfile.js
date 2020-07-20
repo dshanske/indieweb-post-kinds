593cdb241b7305fc240e253ea0ec6f5f29a27909
David Shanske <david@shanske.com>
2020-07-20 03:27:28 +0000

    Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,31 +50,15 @@ module.exports = function(grunt) {
           'css/kind.admin.min.css': 'sass/admin.scss',       // 'destination': 'source'
         }
       }
-    },
-
-    makepot: {
-      target: {
-        options: {
-          mainFile: 'indieweb-post-kinds.php', // Main project file.
-          domainPath: '/languages',                   // Where to save the POT file.
-          potFilename: 'indieweb-post-kinds.pot',
-          type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
-          exclude: [
-            'build/.*'
-          ],
-          updateTimestamp: true             // Whether the POT-Creation-Date should be updated without other changes.
-            	}
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
-  grunt.loadNpmTasks('grunt-wp-i18n');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-eslint');
 
   // Default task(s).
-  grunt.registerTask('default', ['copy', 'wp_readme_to_markdown', 'makepot', 'sass', 'eslint']);
+  grunt.registerTask('default', ['copy', 'wp_readme_to_markdown', 'sass', 'eslint']);
 
 };
