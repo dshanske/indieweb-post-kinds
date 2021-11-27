519573c53e14f5bf12e72dcbde2eb64a903d3032
David Shanske <david@shanske.com>
2021-11-27 05:45:22 +0000

    Add polyfill

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', __DIR__ . '/../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
