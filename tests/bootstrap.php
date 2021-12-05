cebceb0391ed8bb44cdb0a7ef7e212c74d1bd36a
David Shanske <david@shanske.com>
2021-12-05 19:32:07 +0000

    Update tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,15 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
-define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', __DIR__ . '/../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
+$_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
+if ( false !== $_phpunit_polyfills_path ) {
+	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
+}
 
 require_once $_tests_dir . '/includes/functions.php';
+
+require_once dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/indieweb-post-kinds.php';
